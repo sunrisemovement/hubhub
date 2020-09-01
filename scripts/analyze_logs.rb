d2dd6e628af0ee2d395aa612302d4e09962b05b6
Andrew Ross <andrewslavinross@gmail.com>
2020-09-01 09:33:34 -0400

    Log analysis update

--- scripts/analyze_logs.rb
+++ scripts/analyze_logs.rb
@@ -46,10 +46,16 @@ leader_edits = Hash.new { |h,k| h[k] = 0 }
 email_updates = Hash.new { |h,k| h[k] = 0 }
 micro_updates = Hash.new { |h,k| h[k] = 0 }
 
+logs_by_id = Hash.new { |h,k| h[k] = [] }
+
+events = []
+
 logs.each do |log|
   entry = log[-1]
   next unless entry.include?('INFO -')
   next unless hub_id = entry[/Hub (\w+)/, 1]
+
+  logs_by_id[hub_id] << entry
 
   if entry.include?('Attempting login')
     next unless hub_name = entry[/Hub \w+ \(([^\)]+)\)/, 1]
@@ -62,18 +68,39 @@ logs.each do |log|
     hub_name = (ids_to_hub_names[hub_id] || []).last
   end
 
-  if entry.include?('Login successful')
+  if entry.include?("Attempting login")
+    events << { hub_id: hub_id, type: 'login-attempt', entry: entry, date: log[1], ip: log[5] }
+  elsif entry.include?('Login successful')
     login_successes[hub_name] += 1
+    events << { hub_id: hub_id, type: 'login-success', entry: entry, date: log[1], ip: log[5] }
   elsif entry.include?('Editing map info')
     map_info_edits[hub_name] += 1
+    events << { hub_id: hub_id, type: 'map-edit', entry: entry, date: log[1], ip: log[5] }
   elsif entry.include?('Editing leader info')
     leader_edits[hub_name] += 1
+    events << { hub_id: hub_id, type: 'leader-edit', entry: entry, date: log[1], ip: log[5] }
   elsif entry.include?('Updated hub email')
     email_updates[hub_name] += 1
+    events << { hub_id: hub_id, type: 'email-edit', entry: entry, date: log[1], ip: log[5] }
   elsif entry.include?('Editing microsite info')
     micro_updates[hub_name] += 1
+    events << { hub_id: hub_id, type: 'microsite-edit', entry: entry, date: log[1], ip: log[5] }
   end
 end
+
+events.each do |event|
+  event[:hub_name] = (ids_to_hub_names[event[:hub_id]] || []).last
+end
+
+cols = [:hub_id, :hub_name, :type, :date, :ip]
+
+require 'csv'
+File.write("#{LOG_DIR}/events.csv", CSV.generate { |csv|
+  csv << cols
+  events.each do |ev|
+    csv << ev.values_at(*cols)
+  end
+})
 
 binding.pry
 
